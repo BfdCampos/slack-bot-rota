0d6011576221b9494f42c520b78f54800e25f29c
Bruno Campos <brunofdcampos@hotmail.com>
2023-05-22 22:27:27 +0900

    chore: socket mode added

--- app.js
+++ app.js
@@ -7,18 +7,17 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 })
 
-// Logging for debugging
-app.use(async ({ logger, next }) => {
-  logger.info('Received a message')
-  await next()
-})
-
-app.event('app_mention', async ({ event, say }) => {
-  console.log(`Got message from user ${event.user}: ${event.text}`)
-  await say(`Hello, <@${event.user}>`)
+app.message('hello', async ({ message, say }) => {
+  console.log('Message received: ', message)
+  await say(`Hey there <@${message.user}>!`)
 });
 
 (async () => {
-  await app.start(process.env.PORT || 3000)
-  console.log('⚡️ Bolt app is running!')
+  // Start your app
+  try {
+    await app.start(process.env.PORT || 3000)
+    console.log('⚡️ Bolt app is running!')
+  } catch (error) {
+    console.error('Error starting app:', error)
+  }
 })()
