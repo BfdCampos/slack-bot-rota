a78dcd9d5a6128a90a9c0672fbcca7fb081dfa98
Bruno Campos <brunofdcampos@hotmail.com>
2023-05-23 00:14:58 +0900

    chore: separated the rota class for easier dev

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const fs = require('fs')
 const csvParser = require('csv-parser')
 const createCsvWriter = require('csv-writer').createObjectCsvWriter
 
+const Rota = require('./rota')
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   receiver: new SocketModeReceiver({
@@ -16,101 +18,6 @@ const app = new App({
 app.message('hello', async ({ message, say }) => {
   await say(`Hello <@${message.user}>`)
 })
-
-// app.js
-
-class Rota {
-  constructor () {
-    this.users = []
-    this.csvWriter = createCsvWriter({
-      path: 'users.csv',
-      header: [
-        { id: 'username', title: 'username' },
-        { id: 'duty_days', title: 'duty_days' }
-      ]
-    })
-  }
-
-  add (username) {
-    const user = this.users.find((user) => user.username === username)
-    if (!user) {
-      this.users.push({ username, duty_days: 0 })
-      return `Added ${username} to the rota.`
-    } else {
-      return `${username} is already in the rota.`
-    }
-  }
-
-  remove (user) {
-    const index = this.users.indexOf(user)
-    if (index > -1) {
-      this.users.splice(index, 1)
-      return `Removed ${user} from the rota.`
-    } else {
-      return `${user} is not in the rota.`
-    }
-  }
-
-  list () {
-    if (this.users.length === 0) {
-      return 'The rota is currently empty.'
-    } else {
-      return `Rota: ${this.users.join(', ')}`
-    }
-  }
-
-  getCurrentUser () {
-    if (this.users.length === 0) {
-      return 'No one is on duty today.'
-    }
-
-    // Sort the users by the number of duty days (ascending)
-    const sortedUsers = [...this.users].sort(
-      (a, b) => a.duty_days - b.duty_days
-    )
-
-    // Select the first user in the sorted list (the user with the fewest duty days)
-    const user = sortedUsers[0]
-
-    // Increment the number of duty days for this user
-    user.duty_days++
-
-    // Save the updated user data
-    this.save()
-
-    return `Today's duty is on <@${user.username}>.`
-  }
-
-  reset () {
-    for (const user of this.users) {
-      user.duty_days = 0
-    }
-    this.save()
-  }
-
-  save () {
-    return this.csvWriter.writeRecords(this.users)
-  }
-
-  load () {
-    return new Promise((resolve, reject) => {
-      const users = []
-      fs.createReadStream('users.csv')
-        .pipe(csvParser())
-        .on('data', (row) => {
-          users.push({
-            username: row.username,
-            duty_days: Number(row.duty_days)
-          })
-        })
-        .on('end', () => {
-          this.users = users
-          resolve()
-        })
-        .on('error', reject)
-    })
-  }
-}
 
 const rota = new Rota();
 
@@ -203,3 +110,5 @@ async function announceRota (channelId) {
 
 // Start the announcement schedule
 scheduleRotaAnnouncement('C058NGRG4ES')
+
+module.exports = app
