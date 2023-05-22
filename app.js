3d2bc2bf6cb38bcaf9a05ea4b83d9c034f7f385c
Bruno Campos <brunofdcampos@hotmail.com>
2023-05-22 20:16:25 +0900

    fix: go for simpler app to debug issue

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
 // Import required libraries
-require('dotenv').config();
-const { App } = require('@slack/bolt');
+require('dotenv').config()
+const { App } = require('@slack/bolt')
 
-// Initialise your app with your bot token and signing secret
+// Initialises your app with your bot token and signing secret
 const app = new App({
-	token: process.env.SLACK_BOT_TOKEN,
-	signingSecret: process.env.SLACK_SIGNING_SECRET
-});
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+})
 
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
-	// say() sends a message to the channel where the event was triggered
-	await say({
-		blocks: [
-			{
-				type: 'section',
-				text: {
-					type: 'mrkdwn',
-					text: `Hey there <@${message.user}>!`
-				},
-				accessory: {
-					type: 'button',
-					text: {
-						type: 'plain_text',
-						text: 'Click Me'
-					},
-					action_id: 'button_click'
-				}
-			}
-		],
-		text: `Hey there <@${message.user}>!`
-	});
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`)
 });
 
 (async () => {
-	// Start your app
-	await app.start(process.env.PORT || 3000);
+  // Start your app
+  await app.start(process.env.PORT || 3000)
 
-	console.log('⚡️ Bolt app is running!');
-})();
-
+  console.log('⚡️ Bolt app is running!')
+})()
