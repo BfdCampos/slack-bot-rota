1e3b61067b63344552841e71826ce8b75ee054bd
Bruno Campos <brunofdcampos@hotmail.com>
2023-05-22 23:05:00 +0900

    feat: lots of stuff, app function, csv package

--- app.js
+++ app.js
@@ -1,23 +1,142 @@
-const { App } = require('@slack/bolt')
-
 require('dotenv').config()
+
+const { App, LogLevel, SocketModeReceiver } = require('@slack/bolt')
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  receiver: new SocketModeReceiver({
+    appToken: process.env.SLACK_APP_TOKEN
+  }),
+  logLevel: LogLevel.DEBUG
 })
 
 app.message('hello', async ({ message, say }) => {
-  console.log('Message received: ', message)
-  await say(`Hey there <@${message.user}>!`)
+  await say(`Hello <@${message.user}>`)
 });
 
 (async () => {
   // Start your app
-  try {
-    await app.start(process.env.PORT || 3000)
-    console.log('⚡️ Bolt app is running!')
-  } catch (error) {
-    console.error('Error starting app:', error)
-  }
+  await app.start()
+  console.log('⚡️ Bolt app is running!')
 })()
+
+// app.js
+
+class Rota {
+  constructor () {
+    this.users = []
+  }
+
+  add (user) {
+    if (!this.users.includes(user)) {
+      this.users.push(user)
+      return `Added ${user} to the rota.`
+    } else {
+      return `${user} is already in the rota.`
+    }
+  }
+
+  remove (user) {
+    const index = this.users.indexOf(user)
+    if (index > -1) {
+      this.users.splice(index, 1)
+      return `Removed ${user} from the rota.`
+    } else {
+      return `${user} is not in the rota.`
+    }
+  }
+
+  list () {
+    if (this.users.length === 0) {
+      return 'The rota is currently empty.'
+    } else {
+      return `Rota: ${this.users.join(', ')}`
+    }
+  }
+
+  getCurrentUser () {
+    if (this.users.length === 0) {
+      return 'No one is on duty today.'
+    }
+    const today = new Date()
+    const index = today.getDate() % this.users.length
+    return `Today's duty is on <@${this.users[index]}>.`
+  }
+}
+
+// Create an instance of Rota for our app to use
+const rota = new Rota()
+
+app.command('/test_announce_rota', async ({ command, ack, respond }) => {
+  // Acknowledge command request
+  await ack()
+
+  try {
+    await announceRota(command.channel_id)
+    await respond('Announcement sent!')
+  } catch (error) {
+    await respond(`Failed to send announcement: ${error.message}`)
+  }
+})
+
+app.command('/rota', async ({ command, ack, respond }) => {
+  await ack()
+
+  // Parse the text of the command to determine what action to take
+  const action = command.text.split(' ')[0]
+  const user = command.text.split(' ')[1]
+
+  let responseText = ''
+
+  if (action === 'add') {
+    responseText = rota.add(user)
+  } else if (action === 'remove') {
+    responseText = rota.remove(user)
+  } else if (action === 'list') {
+    responseText = rota.list()
+  } else {
+    responseText =
+      "Sorry, I don't understand that command. Try /rota add @user, /rota remove @user, or /rota list."
+  }
+
+  await respond({ text: responseText })
+})
+
+// Send the current rota user to a channel every day at a specific time
+function scheduleRotaAnnouncement (channelId) {
+  // Calculate time until next announcement
+  const now = new Date()
+  const tomorrow = new Date(now)
+  tomorrow.setDate(tomorrow.getDate() + 1)
+  tomorrow.setHours(13, 56, 0, 0) // 9 AM
+  const msUntilTomorrow = tomorrow - now
+
+  // Schedule first announcement
+  setTimeout(() => {
+    announceRota(channelId)
+
+    // Schedule all following announcements
+    setInterval(() => {
+      announceRota(channelId)
+    }, 24 * 60 * 60 * 1000) // Every 24 hours
+  }, msUntilTomorrow)
+}
+
+// Send a message to a channel with the current rota user
+async function announceRota (channelId) {
+  try {
+    console.log('Trying to announce rota...')
+    // Use chat.postMessage method to send a message from your app
+    const result = await app.client.chat.postMessage({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: channelId,
+      text: rota.getCurrentUser()
+    })
+    console.log('Rota announced:', result)
+  } catch (error) {
+    console.error('Error announcing rota:', error)
+  }
+}
+
+// Start the announcement schedule
+scheduleRotaAnnouncement('C058NGRG4ES')
